35c33854d78a989c87451c0c980a7b422f7081d4
Benedictus-Kent Rachmat <benedictuskent.rachmat.etu@univ-lille.fr>
2021-01-21 20:17:28 +0100

    new

---
+++ math/README.md
@@ -0,0 +1,1 @@
+# 15Math4Kids
---
+++ math/script.js
@@ -0,0 +1,57 @@
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+var x = document.getElementById("myAudio"); 
+var answer = 0;
+
+function random_equation(){ 
+  var val1 = Math.floor(Math.random() * 13); 
+  var val2 = Math.floor(Math.random() * 13);
+  var dummyAnswer1 = Math.floor(Math.random() * 10);
+  var dummyAnswer2 = Math.floor(Math.random() * 10);
+  var random = [];
+  answer = eval(val1 + val2);
+  
+  document.getElementById("value1").innerHTML = val1; 
+  document.getElementById("value2").innerHTML = val2; 
+
+  var randomAnswers = [answer, dummyAnswer1, dummyAnswer2];
+
+  for (var randomAnswers = [answer, dummyAnswer1, dummyAnswer2], i = randomAnswers.length; i--;){
+    random.push(randomAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+  };
+
+  option1.innerHTML = random[0];
+  option2.innerHTML = random[1];
+  option3.innerHTML = random[2]; 
+
+};
+
+option1.addEventListener("click", function(){
+    if(option1.innerHTML == answer){
+      random_equation();
+    }
+    else{
+      x.play();
+    }
+});
+option2.addEventListener("click", function(){
+    if(option2.innerHTML == answer){
+      random_equation();
+    }
+    else{
+      x.play();
+    }
+});
+option3.addEventListener("click", function(){
+    if(option3.innerHTML == answer){
+      random_equation();
+    }
+    else{
+      x.play();
+    }
+});
+
+random_equation()
+
+
